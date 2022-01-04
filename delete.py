c2ba84d8aec9a3ac290cc9d730aba14aab5f457c
Yusuf Rafif Karback <yusufkarback@Yusufs-MacBook-Pro.local>
2022-01-04 21:09:21 +0700

    update delete.py

--- delete.py
+++ delete.py
@@ -2,30 +2,24 @@ import mysqlconnected as mysql_connection
 
 
 def delete_data(id):
-    try:
-        # koneksi ke database
-        db = mysql_connection.connect_sql()
-        cursor = db.cursor()
-        print("Connected to MYSQL")
 
-        # menjalankan query delete
-        delete = "DELETE FROM transaksi_travel WHERE id = %s"
+    # koneksi ke database
+    db = mysql_connection.connect_sql()
+    cursor = db.cursor()
+    print("Connected to MYSQL")
 
-        cursor.execute(delete, (id, ))
-        db.commit()
-        if cursor.rowcount <= 0:
-            print('maaf id yang dimasukan salah')
-            print("{} data diubah".format(cursor.rowcount))
-        else:
-            print('Hapus data sukses')
-            print("{} data diubah".format(cursor.rowcount))
+    # menjalankan query delete
+    delete = "DELETE FROM transaksi_travel WHERE id = %s"
 
-        cursor.close()
+    cursor.execute(delete, (id, ))
+    db.commit()
+    if cursor.rowcount <= 0:
+        print('maaf id yang dimasukan salah')
+        print("{} data diubah".format(cursor.rowcount))
+    else:
+        print('Hapus data sukses')
+        print("{} data diubah".format(cursor.rowcount))
 
-    # error handling jika terjadi error
-    except mysql_connection.mysql_error() as error:
-        print("gagal terkoneksi ke tabel", error)
-    finally:
-        if (db):
-            db.close()
-            print("koneksi mysql Selesai")
+    cursor.close()
+
+# error handling jika terjadi error
